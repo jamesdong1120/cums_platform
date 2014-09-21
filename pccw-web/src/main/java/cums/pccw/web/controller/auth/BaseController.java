b883a265fa989c04bc7014aab05233a99a7db1c3
jamesdong1120 <527804131@qq.com>
2014-09-21 21:45:19 +0800

    first

---
+++ pccw-web/src/main/java/cums/pccw/web/controller/auth/BaseController.java
@@ -0,0 +1,34 @@
+package cums.pccw.web.controller.auth;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setAutoGrowCollectionLimit(Integer.MAX_VALUE);
+
+        //CustomDateEditor 可以换成自己定义的编辑器。
+        //注册一个Date 类型的绑定器 。
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+}
